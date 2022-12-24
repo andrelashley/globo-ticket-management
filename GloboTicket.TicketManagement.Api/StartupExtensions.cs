47e0b474aff92029cbeaba7e4ce866846c0768e1
André Lashley <andre.lashley@gmail.com>
2022-12-23 17:38:38 -0800

    Add Swagger support

--- GloboTicket.TicketManagement.Api/StartupExtensions.cs
+++ GloboTicket.TicketManagement.Api/StartupExtensions.cs
@@ -1,7 +1,9 @@
-﻿using GloboTicket.TicketManagement.Application;
+﻿using GloboTicket.TicketManagement.Api.Utility;
+using GloboTicket.TicketManagement.Application;
 using GloboTicket.TicketManagement.Infrastructure;
 using GloboTicket.TicketManagement.Persistence;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 
 namespace GloboTicket.TicketManagement.Api
 {
@@ -10,6 +12,8 @@ namespace GloboTicket.TicketManagement.Api
         public static WebApplication ConfigureServices(
         this WebApplicationBuilder builder)
         {
+            AddSwagger(builder.Services);
+
             builder.Services.AddApplicationServices();
             builder.Services.AddInfrastructureServices(builder.Configuration);
             builder.Services.AddPersistenceServices(builder.Configuration);
@@ -26,6 +30,14 @@ namespace GloboTicket.TicketManagement.Api
 
         public static WebApplication ConfigurePipeline(this WebApplication app)
         {
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI(c =>
+                {
+                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "GloboTicket Ticket Management API");
+                });
+            }
 
             app.UseHttpsRedirection();
 
@@ -36,6 +48,21 @@ namespace GloboTicket.TicketManagement.Api
             app.MapControllers();
 
             return app;
+        }
+
+        private static void AddSwagger(IServiceCollection services)
+        {
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "GloboTicket Ticket Management API",
+
+                });
+
+                c.OperationFilter<FileResultContentTypeOperationFilter>();
+            });
         }
 
         public static async Task ResetDatabaseAsync(this WebApplication app)
